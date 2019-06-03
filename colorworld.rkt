5c63390dabb08ce516fded2ec3db9204132d1230
Ryan Herbrechtsmeier <rherbrechtsmeier@nextekdevelopment.com>
2019-06-03 16:09:18 -0500

    Using matrix instead of nested lists
    Unsure this is any use considering the action will be identical considering

--- colorworld.rkt
+++ colorworld.rkt
@@ -2,6 +2,7 @@
 
 (require
   2htdp/image
+  htdp/matrix
   "colorcell.rkt")
 
 (provide colorworld%)
@@ -11,10 +12,21 @@
     (init-field
      [row 1]
      [col 1])
-    (define color-cells (for/list ([i (in-range row)])
-                          (for/list ([j (in-range col)])
-                            (new colorcell%))))
+    ;;; Using matrix since code uses bst search which looks
+    ;; to be faster on look up
+    (define color-matrix
+      (make-matrix
+       col
+       row
+       (for/list ([i (in-range (* col row))])
+         (new colorcell%))))
+    (define/public (get-color-cells) color-cells)
     ;;; TODO Add draw logic here
     (define/public (draw scene)
       (void))
     (super-new)))
+
+(define my-world (new colorworld%))
+(define mt-sc (empty-scene 0 0))
+
+(send my-world draw mt-sc)
